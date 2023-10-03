1c48955be424f251ede2968b07832c1022ee394e
Tomek <tomek.sobolewski@onereach.com>
2023-10-03 18:17:36 +0200

    allow to add and remove passkeys from user profile

--- assets/js/UserSettings.ts
+++ assets/js/UserSettings.ts
@@ -1,13 +1,35 @@
 import {RegistrationHandler} from "./RegistrationHandler";
+import {Utilities} from "./Utilities";
+import {VerificationResponse} from "./AuthenticatorInterface";
 
 class UserSettings {
 
-    private registerSuccessNotification: HTMLElement | null = document.querySelector('.register-success-notification');
-    private registerErrorNotification: HTMLElement | null = document.querySelector('.register-error-notification');
+    private readonly registerSuccessNotification: HTMLElement | null = document.querySelector('.register-success-notification');
+    private readonly registerErrorNotification: HTMLElement | null = document.querySelector('.register-error-notification');
+    private readonly passkeysRegisterButton = document.querySelector<HTMLInputElement>('.passkeys-login__button--add');
+    private readonly passkeysRemoveButton = document.querySelector<HTMLInputElement>('.passkeys-login__button--remove');
+    private readonly passkeyRow = document.querySelector('#pk_credential_id');
 
-    async startRegistration(): Promise<void> {
+
+    async startRegistration(): Promise<VerificationResponse> {
         const regHandler = new RegistrationHandler(this.notify.bind(this));
-        await regHandler.start();
+        return await regHandler.start();
+    }
+
+    async removePasskey(): Promise<void> {
+        try {
+            // @ts-ignore
+            const response: Response = await fetch('/wp-json/wp-passkeys/creds/user/remove', {
+                method: 'DELETE',
+                headers: {
+                    'X-WP-Nonce': passkeys.nonce,
+                }
+            });
+            return await response.json();
+        } catch (error: any) {
+            console.error("User credentials has not been removed:", error);
+            throw error;
+        }
     }
 
     public notify(isSuccess: boolean, message: string): void {
@@ -19,10 +41,31 @@ class UserSettings {
     }
 
     public attachEventListeners(): void {
-        const passkeysRegisterButton = document.querySelector<HTMLInputElement>('.passkeys-login__button--add');
-        if (passkeysRegisterButton) {
-            passkeysRegisterButton.addEventListener('click', () => {
-                this.startRegistration().then(r => console.log(r));
+        if (this.passkeysRegisterButton) {
+            this.passkeysRegisterButton.addEventListener('click', () => {
+                Utilities.setUserLogin().then(() => {
+                    this.startRegistration().then((r) => {
+                        this.notify(true, 'Passkey has been registered. You can now login without password!');
+                        console.log(r);
+                        console.log(r.pk_credential_id);
+                        if (this.passkeyRow) {
+                            this.passkeyRow.innerHTML = <string>r.pk_credential_id;
+                        }
+                    })
+                });
+            });
+        }
+
+        if (this.passkeysRemoveButton) {
+            this.passkeysRemoveButton.addEventListener('click', () => {
+                this.removePasskey().then(
+                    () => {
+                        if (this.passkeyRow) {
+                            this.passkeyRow.innerHTML = '';
+                        }
+                        this.notify(true, 'Passkey has been removed.');
+                    }
+                );
             });
         }
     }
