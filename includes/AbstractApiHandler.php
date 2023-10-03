53a136de8a41e961587cf7f288a41be1885e44e3
Tomek <tomek.sobolewski@onereach.com>
2023-10-03 18:17:49 +0200

    add permission callback argument for api client

--- includes/AbstractApiHandler.php
+++ includes/AbstractApiHandler.php
@@ -4,15 +4,19 @@ namespace WpPasskeys;
 
 abstract class AbstractApiHandler
 {
-    protected function registerRoute(string $endpoint, string $method, array $callback): void
-    {
+    protected function registerRoute(
+        string $endpoint,
+        string $method,
+        array $callback,
+        callable | string|null $permission = null
+    ): void {
         register_rest_route(
             WP_PASSKEYS_API_NAMESPACE,
             $endpoint,
             array(
                 'methods'  => $method,
                 'callback' => $callback,
-                'permission_callback' => '__return_true',
+                'permission_callback' => $permission ?? '__return_true',
             )
         );
     }
