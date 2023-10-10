f24f2064ad2267c090f035a8485eb3a4d0aecb33
Tomek <tomek.sobolewski@onereach.com>
2023-10-10 18:13:15 +0200

    preparing environment for unit & integration tests, added brain monkey and couple sample tests

---
+++ tests/TestCase.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace WpPasskeys\Tests;
+
+use function Brain\Monkey\setUp;
+use function Brain\Monkey\tearDown;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+use Brain\Monkey\Functions;
+class TestCase extends PHPUnitTestCase {
+
+    // Adds Mockery expectations to the PHPUnit assertions count.
+    use MockeryPHPUnitIntegration;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        setUp();
+        Functions\when('add_options_page')->justReturn(true);
+    }
+
+    protected function tearDown(): void
+    {
+        tearDown();
+        parent::tearDown();
+    }
+}
